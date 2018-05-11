bc68542bb2174a2c27dacf9cb2280e21f22b1bf4
Andy Li <andyli20s@gmail.com>
2018-05-11 10:57:11 -0400

    note form

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,4 +3,12 @@ class MainController < ApplicationController
   def home 
     @note = "This is a note."
   end
+
+  def note_form
+  end
+
+  def note_form_submit
+    # TODO: create note in db
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to => "main#home"
+
+  get  "/note_form" => "main#note_form"
+  post "/note_form_submit" => "main#note_form_submit"
 end
