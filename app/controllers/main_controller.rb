43222b54015851742c4b441036c6279e91fa8685
Andy Li <andyli20s@gmail.com>
2018-05-11 11:04:02 -0400

    create note in db

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,14 +1,15 @@
 class MainController < ApplicationController
 
   def home 
-    @note = "This is a note."
+    @note = Note.first
   end
 
   def note_form
   end
 
   def note_form_submit
-    # TODO: create note in db
+    note_content = params[:note_content]
+    Note.create({ content: note_content })
     redirect_to root_path
   end
 end
